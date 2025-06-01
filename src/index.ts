3d3f28d59b9aa06665be90e23a28310effed14ea
mryhryki <mryhryki@gmail.com>
2025-06-01 09:12:00 +0900

    Create example / exampleの作成

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,33 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Bind resources to your worker in `wrangler.jsonc`. After adding bindings, a type definition for the
- * `Env` object can be regenerated with `npm run cf-typegen`.
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
+export type SessionHandler = (request: Request, env: Env, ctx: ExecutionContext) => Promise<Response>
 
-export default {
-  async fetch(request, env, ctx): Promise<Response> {
-    return new Response("Hello World!");
+export interface InitSessionHandlerArgs {
+  oidc: {
+    clientId: string
+    clientSecret: string
+    baseDomain: string
   },
-} satisfies ExportedHandler<Env>;
+  secret: {
+    signingKey: string
+  }
+  onRequestWithValidSession: (
+    request: Request,
+    env: Env,
+    ctx: ExecutionContext,
+    user: unknown, /* TODO */
+  ) => Promise<Response>
+}
+
+export const initSessionHandler = (args: InitSessionHandlerArgs): SessionHandler => {
+  const { onRequestWithValidSession} = args
+
+  // TODO
+
+  const user: unknown = {}; // TODO
+
+  return async (request, env, ctx) => {
+
+    // TODO
+
+    return onRequestWithValidSession(request, env, ctx, user)
+  }
+}
