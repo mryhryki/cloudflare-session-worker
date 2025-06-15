c6e4207f21dbedb709cb7e10ce43cad8503181a8
mryhryki <mryhryki@gmail.com>
2025-06-15 17:37:05 +0900

    "Improve logoutHandler() / logoutHandler()を改善"

--- src/handlers/logout.ts
+++ src/handlers/logout.ts
@@ -1,14 +1,8 @@
 import type { KVNamespace } from "@cloudflare/workers-types";
 import { getOidcConfiguration } from "../lib/oidc/configucation.ts";
-import { oidcRequestHandler } from "../lib/oidc/odic_request.ts";
 import { getSessionId } from "../lib/session_store/cookie/get.ts";
-import { createSessionStore } from "../lib/session_store/create.ts";
 import { getSessionStore } from "../lib/session_store/get.ts";
-import type {
-  OidcParams,
-  SessionConfiguration,
-  SessionPaths,
-} from "../types.ts";
+import type { OidcParams, SessionConfiguration } from "../types.ts";
 import { isInLocalDevelopment } from "../util/request.ts";
 import { forceSameOrigin } from "../util/url.ts";
 
@@ -24,27 +18,42 @@ export const logoutHandler = async (
 ): Promise<Response> => {
   const { req, config, kv, oidcParams } = args;
 
+  const defaultReturnTo = forceSameOrigin(config.defaultReturnTo, req.url);
+  const defaultResponse = new Response(`Redirect to ${defaultReturnTo}`, {
+    status: 307,
+    headers: { Location: defaultReturnTo, "Content-Type": "text/plan" },
+  });
+
+  const sessionId = getSessionId(req, config.cookieName);
+  if (typeof sessionId !== "string") {
+    return defaultResponse;
+  }
+
+  const sessionStore = await getSessionStore({
+    config,
+    useSecureCookie: !isInLocalDevelopment(req),
+    kv,
+    sessionId,
+  });
+  const session = await sessionStore.get();
+  if (session?.status !== "logged-in") {
+    return defaultResponse;
+  }
+
   const oidcConfiguration = await getOidcConfiguration(oidcParams);
-  const { end_session_endpoint: endSessionEndpoint } =
-    oidcConfiguration.serverMetadata();
-  const redirectTo =
-    endSessionEndpoint ?? forceSameOrigin(config.defaultReturnTo, req.url);
+  const { end_session_endpoint } = oidcConfiguration.serverMetadata();
+  if (typeof end_session_endpoint !== "string") {
+    return defaultResponse;
+  }
+
+  const url = new URL(end_session_endpoint);
+  url.searchParams.set("id_token_hint", session.idToken);
+  const redirectTo = url.href;
 
   const response = new Response(`Redirect to: ${redirectTo}`, {
     status: 307,
     headers: { Location: redirectTo },
   });
-
-  const sessionId = getSessionId(req, config.cookieName);
-  if (typeof sessionId === "string") {
-    const sessionStore = await getSessionStore({
-      config,
-      useSecureCookie: !isInLocalDevelopment(req),
-      kv,
-      sessionId,
-    });
-    await sessionStore.delete(response);
-  }
-
+  await sessionStore.delete(response);
   return response;
 };
